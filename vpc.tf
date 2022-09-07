4af75ba97b0562949b3665c7d75f9bb8a4868929
rwtobi01 <86566443+rwtobi01@users.noreply.github.com>
2022-09-07 14:12:06 +0100

    add vpc

---
+++ vpc.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "fashion" {
+  cidr_block = "192.168.0.0/16"
+
+  tags = {
+    Name = "fashion"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.fashion.id
+  cidr_block = "192.168.11.0/24"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.fashion.id
+  cidr_block = "192.168.12.0/24"
+
+  tags = {
+    Name = "private_subnet"
+  }
+}
+
+  resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.fashion.id
+  cidr_block = "192.168.13.0/24"
+
+  tags = {
+    Name = "private_subnet_1"
+  }
+}
