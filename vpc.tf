3ea3f375220d75a3afd98cb451fb1ff80ed0c964
rwtobi01 <86566443+rwtobi01@users.noreply.github.com>
2022-09-07 17:08:57 +0100

    update

--- vpc.tf
+++ vpc.tf
@@ -1,35 +1,35 @@
-resource "aws_vpc" "fashion" {
-  cidr_block = "192.168.0.0/16"
+# resource "aws_vpc" "fashion" {
+#   cidr_block = "192.168.0.0/16"
 
-  tags = {
-    Name = "fashion"
-  }
-}
+#   tags = {
+#     Name = "fashion"
+#   }
+# }
 
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.fashion.id
-  cidr_block = "192.168.11.0/24"
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id     = aws_vpc.fashion.id
+#   cidr_block = "192.168.11.0/24"
 
-  tags = {
-    Name = "public_subnet"
-  }
-}
+#   tags = {
+#     Name = "public_subnet"
+#   }
+# }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.fashion.id
-  cidr_block = "192.168.12.0/24"
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id     = aws_vpc.fashion.id
+#   cidr_block = "192.168.12.0/24"
 
-  tags = {
-    Name = "private_subnet"
-  }
-}
+#   tags = {
+#     Name = "private_subnet"
+#   }
+# }
 
-  resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.fashion.id
-  cidr_block = "192.168.13.0/24"
+# resource "aws_subnet" "private_subnet_1" {
+#   vpc_id     = aws_vpc.fashion.id
+#   cidr_block = "192.168.13.0/24"
 
-  tags = {
-    Name = "private_subnet_1"
-  }
-}
+#   tags = {
+#     Name = "private_subnet_1"
+#   }
+# }
